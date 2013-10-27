cfb42be5f3782b15bf2d1de758e09642896fc07e
Jason Roth <jasonmichaelroth@gmail.com>
2013-10-27 12:49:13 -0600

    refactor tests

--- test/solar_noon_test.rb
+++ test/solar_noon_test.rb
@@ -18,36 +18,24 @@ class SolarNoonTest < Minitest::Spec
   end
 
   test "DST time - 2011-03-26 11:42:36 MDT at -105.0" do
-    noon = Date.civil(2011, 3, 26).solar_noon(-105.0)
-    assert_within_one_minute DateTime.civil(2011, 3, 26, 19, 5, 00, 'UTC'), noon
-
-    noon = DateTime.civil(2011, 3, 26, 11, 42, 36).solar_noon(-105.0)
-    assert_within_one_minute DateTime.civil(2011, 3, 26, 19, 5, 00, 'UTC'), noon
-
-    noon = Time.local(2011, 3, 26, 11, 42, 36).solar_noon(-105.0)
-    assert_within_one_minute DateTime.civil(2011, 3, 26, 19, 5, 00, 'UTC'), noon
+    expected = DateTime.civil(2011, 3, 26, 19, 5, 00, 'UTC')
+    assert_within_one_minute expected, Date.civil(2011, 3, 26).solar_noon(-105.0)
+    assert_within_one_minute expected, DateTime.civil(2011, 3, 26, 11, 42, 36).solar_noon(-105.0)
+    assert_within_one_minute expected, Time.local(2011, 3, 26, 11, 42, 36).solar_noon(-105.0)
   end
 
   test "UTC time - 2001-06-12 19:18:15 UTC at 82.5" do
-    noon = Date.civil(2001, 6, 12).solar_noon(82.5)
-    assert_within_one_minute DateTime.civil(2001, 6, 12, 6, 29, 00, 'UTC'), noon
-
-    noon = DateTime.civil(2001, 6, 12, 19, 18, 15, "UTC").solar_noon(82.5)
-    assert_within_one_minute DateTime.civil(2001, 6, 12, 6, 29, 00, 'UTC'), noon
-
-    noon = Time.utc(2001, 6, 12, 19, 18, 15).solar_noon(82.5)
-    assert_within_one_minute DateTime.civil(2001, 6, 12, 6, 29, 00, 'UTC'), noon
+    expected = DateTime.civil(2001, 6, 12, 6, 29, 00, 'UTC')
+    assert_within_one_minute expected, Date.civil(2001, 6, 12).solar_noon(82.5)
+    assert_within_one_minute expected, DateTime.civil(2001, 6, 12, 19, 18, 15, "UTC").solar_noon(82.5)
+    assert_within_one_minute expected, Time.utc(2001, 6, 12, 19, 18, 15).solar_noon(82.5)
   end
 
   test "future time - 2031-11-28 05:07:26 MDT at 132.2" do
-    noon = Date.civil(2031, 11, 28).solar_noon(132.2)
-    assert_within_one_minute DateTime.civil(2031, 11, 28, 2, 59, 00, 'UTC'), noon
-
-    noon = DateTime.civil(2031, 11, 28, 5, 7, 26, "MDT").solar_noon(132.2)
-    assert_within_one_minute DateTime.civil(2031, 11, 28, 2, 59, 00, 'UTC'), noon
-
-    noon = Time.utc(2031, 11, 28, 5, 7, 26).solar_noon(132.2)
-    assert_within_one_minute DateTime.civil(2031, 11, 28, 2, 59, 00, 'UTC'), noon
+    expected = DateTime.civil(2031, 11, 28, 2, 59, 00, 'UTC')
+    assert_within_one_minute expected, Date.civil(2031, 11, 28).solar_noon(132.2)
+    assert_within_one_minute expected, DateTime.civil(2031, 11, 28, 5, 7, 26, "MDT").solar_noon(132.2)
+    assert_within_one_minute expected, Time.utc(2031, 11, 28, 5, 7, 26).solar_noon(132.2)
   end
 
 end
